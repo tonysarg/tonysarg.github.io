b256223c0eb2ce41e09a9067f9ff5f647ea7d160
Tony <tonyksarg@gmail.com>
2019-06-20 00:54:08 -0700

    123

--- js/transitions.js
+++ js/transitions.js
@@ -11,18 +11,18 @@ $(".hero__nav a").on("click", function (event) {
 
 $(document).ready(function(){
     $(".work").click(function(){
-        $(".main").load("index.html .main");
+        $(".main").load("index.html .main1");
     });
 });
 
 $(document).ready(function(){
     $(".resume").click(function(){
-        $(".main").load("resume.html .main");
+        $(".main").load("resume.html .main2");
     });
 });
 
 $(document).ready(function(){
     $(".about").click(function(){
-        $(".main").load("about.html .main");
+        $(".main").load("about.html .main3");
     });
 });
