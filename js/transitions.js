f52c553e21499af07f121c7f2141764673ae02b9
Tony <tonyksarg@gmail.com>
2019-06-19 23:11:58 -0700

    Update transitions.js

--- js/transitions.js
+++ js/transitions.js
@@ -1,32 +1,27 @@
 $(".hero__nav a").on("click", function (event) {
-
-	event.preventDefault()
-
-	const href = $(this).attr('href')
-
-	window.history.pushState(null, null, href)
-
-
+  
+  event.preventDefault()
+  
+  const href = $(this).attr("href")
+  
+  window.history.pushState(null, null, href)
+  
 	$(".hero__nav a").removeClass("hero__nav-link--active")
 	$(".hero__nav a").removeClass("w--current")
 	$(this).addClass("hero__nav-link--active")
 	$(this).addClass("w--current")
-
-
-	$.ajax({
-		url: href,
-		success: function (data) {
-			$(".main").fadeOut(250, function () {
-				const newPage = $(data).filter(".main").html()
+  
+  $.ajax({
+    url: href,
+    success: function (data) {
+      $(".main").fadeOut(250, function () {
+        const newPage = $(data).filter("section").html()
         
-        		$(".main").html(newPage)
-
-
-
-				$(".main").fadeIn(250)
-			})
-		}
-	})
-
-
+        $(".main").html(newPage)
+        
+        $(".main").fadeIn(250)
+      })
+    }
+  })
+  
 })
