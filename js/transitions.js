236fefbb216cdcc5ce4e592c29e775e3023042e4
Tony <tonyksarg@gmail.com>
2019-06-19 23:13:31 -0700

    Update transitions.js

--- js/transitions.js
+++ js/transitions.js
@@ -15,7 +15,7 @@ $(".hero__nav a").on("click", function (event) {
     url: href,
     success: function (data) {
       $(".main").fadeOut(250, function () {
-        const newPage = $(data).filter("section").html()
+        const newPage = $(data).filter(".main").html()
         
         $(".main").html(newPage)
         
